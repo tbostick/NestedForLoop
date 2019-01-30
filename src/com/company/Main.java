70b298b67d2b41cf1918fd8aeb96ae4422b6397b
tbostick <timbostick1950@gmail.com>
2019-01-30 16:51:28 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	   for (int i=1; i<=3; i++) // row
+	    {
+	        for (int k= 1; k<=i; k++)
+	        {
+             System.out.print(i);
+	        }// end for loop 2
+                System.out.println();
+        }//end for loop 1
+    }
+}
